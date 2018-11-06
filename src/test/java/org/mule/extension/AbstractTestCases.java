00c3a8a066054d285fab6b43ae988286296c2ace
Fiordalisi <te.fiordalisi@gmail.com>
2018-11-06 10:54:16 -0300

    many changes

--- src/test/java/org/mule/extension/AbstractTestCases.java
+++ src/test/java/org/mule/extension/AbstractTestCases.java
@@ -1,8 +1,32 @@
 package org.mule.extension;
 
+import org.mule.extension.api.Zwarrior;
 import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
 
-public class AbstractTestCases extends MuleArtifactFunctionalTestCase {
+import static org.mule.extension.TestDataBuilder.generateZWarrior;
+
+public abstract class AbstractTestCases extends MuleArtifactFunctionalTestCase {
+    private static final String FLOW_CONFIG_LOCATION = "src/test/resources/automation-test-flows.xml";
+
+    @Override
+    public int getTestTimeoutSecs() {
+        return 999999;
+    }
+
+    @Override
+    protected String[] getConfigFiles() {
+        return new String[] {
+                FLOW_CONFIG_LOCATION
+        };
+    }
 
 
+   protected Zwarrior executeCreate(Zwarrior zwarrior) throws Exception{
+       Zwarrior zwarriorP = ((Zwarrior) flowRunner("crearZwarriorFlow")
+               .withPayload(zwarrior).run()
+               .getMessage()
+               .getPayload()
+               .getValue());
+       return zwarriorP;
+   }
 }
--- src/test/java/org/mule/extension/DragonBallZOperationsTestCase.java
+++ src/test/java/org/mule/extension/DragonBallZOperationsTestCase.java
@@ -4,76 +4,72 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mule.extension.TestDataBuilder.*;
 
-import org.junit.Assert;
-import org.mule.extension.api.Zwarrior;
-import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
 import org.junit.Test;
 
 
-public class DragonBallZOperationsTestCase extends MuleArtifactFunctionalTestCase {
-
-  /**
-   * Specifies the mule config xml with the flows that are going to be executed in the tests, this file lives in the test resources.
-   */
-  @Override
-  protected String getConfigFile() {
-    return "test-mule-config.xml";
-  }
-
-  @Test
-  public void executeSayHiOperation() throws Exception {
-    String payloadValue = ((String) flowRunner("sayHiFlow").run()
-                                      .getMessage()
-                                      .getPayload()
-                                      .getValue());
-    assertThat(payloadValue, is("Hello Mariano Gonzalez!!!"));
-  }
+public class DragonBallZOperationsTestCase extends AbstractTestCases {
 
 
-  @Test
-  public void executeNoGetZwarriorOperation() throws Exception {
+    protected static final String FLOW_CONFIG_LOCATION = "automation-test-flows.xml";
 
-    String payloadValue = ((String) flowRunner("getZwarriorFlow").run()
-            .getMessage()
-            .getPayload()
-            .getValue());
-    assertThat(payloadValue, is("No se ha encontrado el warrior Goku"));
-  }
+    /**
+     * Specifies the mule config xml with the flows that are going to be executed in the tests, this file lives in the test resources.
+     */
+    @Override
+    protected String getConfigFile() {
+        return "automation-test-flows.xml";
+    }
+
+    @Test
+    public void executeSayHiOperation() throws Exception {
+        String payloadValue = ((String) flowRunner("sayHiFlow").run()
+                .getMessage()
+                .getPayload()
+                .getValue());
+        assertThat(payloadValue, is("Hello Mariano Gonzalez!!!"));
+    }
 
 
-  @Test
-  public void executeCrearZwarriorOperation() throws Exception {
+    @Test
+    public void executeNoGetZwarriorOperation() throws Exception {
 
-    Zwarrior zwarrior = ((Zwarrior) flowRunner("crearZwarriorFlow")
-            .withPayload(generateZWarrior("vegeta", 100)).run()
-            .getMessage()
-            .getPayload()
-            .getValue());
-    assertThat(zwarrior.getNombre(), is(generateZWarrior("vegeta", 100).getNombre()));
+        String payloadValue = ((String) flowRunner("getZwarriorFlow").run()
+                .getMessage()
+                .getPayload()
+                .getValue());
+        assertThat(payloadValue, is("No se ha encontrado el warrior Goku"));
+    }
 
-  }
 
-  @Test
-  public void executeCrearMismoZwarriorOperation() throws Exception {
+    @Test
+    public void executeCreateZwarriorOperation() throws Exception {
 
-    flowRunner("crearZwarriorFlow").run();
-    String payloadValue = ((String) flowRunner("crearZwarriorFlow").run()
-            .getMessage()
-            .getPayload()
-            .getValue());
-    assertThat(payloadValue, is("Ya existe el warrior (?"));
-  }
 
-  @Test
-  public void executeGetZwarriorOperation() throws Exception {
+        assertThat(executeCreate(generateZWarrior("vegeta", 100)).getNombre(), is(generateZWarrior("vegeta", 100).getNombre()));
 
-    flowRunner("crearZwarriorFlow").run();
-    String payloadValue = ((String) flowRunner("getZwarriorFlow").run()
-            .getMessage()
-            .getPayload()
-            .getValue());
-    assertThat(payloadValue, is("Se encontro a Goku con id: 1"));
-  }
+    }
+
+    @Test
+    public void executeCrearMismoZwarriorOperation() throws Exception {
+
+        flowRunner("crearZwarriorFlow").run();
+        String payloadValue = ((String) flowRunner("crearZwarriorFlow").run()
+                .getMessage()
+                .getPayload()
+                .getValue());
+        assertThat(payloadValue, is("Ya existe el warrior (?"));
+    }
+
+    @Test
+    public void executeGetZwarriorOperation() throws Exception {
+
+        flowRunner("crearZwarriorFlow").run();
+        String payloadValue = ((String) flowRunner("getZwarriorFlow").run()
+                .getMessage()
+                .getPayload()
+                .getValue());
+        assertThat(payloadValue, is("Se encontro a Goku con id: 1"));
+    }
 
  /* @Test
   public void executeGetObjectZwarriorOperation() throws Exception {
@@ -85,55 +81,55 @@ public class DragonBallZOperationsTestCase extends MuleArtifactFunctionalTestCas
   }
 */
 
-  @Test
-  public void executeListZwarrior() throws Exception{
-    flowRunner("crearZwarriorFlow").run();
-    flowRunner("crearZwarriorFlow2").run();
-    String payloadValue = ((String) flowRunner("listZwarriorFlow")
-            .run()
-            .getMessage()
-            .getPayload()
-            .getValue());
-    assertThat(payloadValue, is("Los Warriors son: Goku, Gohan"));
-  }
+    @Test
+    public void executeListZwarrior() throws Exception {
+        flowRunner("crearZwarriorFlow").run();
+        flowRunner("crearZwarriorFlow2").run();
+        String payloadValue = ((String) flowRunner("listZwarriorFlow")
+                .run()
+                .getMessage()
+                .getPayload()
+                .getValue());
+        assertThat(payloadValue, is("Los Warriors son: Goku, Gohan"));
+    }
 
-  @Test
-  public void executeModifyWArriorOperation() throws Exception{
-    flowRunner("crearZwarriorFlow").run();
-    flowRunner("crearZwarriorFlow2").run();
-    flowRunner("crearVegeta").run();
-    flowRunner("modifyWarrior").run();
-    String payloadValue =  ((String) flowRunner("getGohan").run()
-    .getMessage()
-    .getPayload()
-    .getValue());
+    @Test
+    public void executeModifyWArriorOperation() throws Exception {
+        flowRunner("crearZwarriorFlow").run();
+        flowRunner("crearZwarriorFlow2").run();
+        flowRunner("crearVegeta").run();
+        flowRunner("modifyWarrior").run();
+        String payloadValue = ((String) flowRunner("getGohan").run()
+                .getMessage()
+                .getPayload()
+                .getValue());
 
-    assertThat(payloadValue, is("No se ha encontrado el warrior Gohan"));
-  }
+        assertThat(payloadValue, is("No se ha encontrado el warrior Gohan"));
+    }
 
-  @Test
-  public void executeDeleteWarriorOperation() throws  Exception{
-    flowRunner("crearZwarriorFlow").run();
-    flowRunner("crearZwarriorFlow2").run();
-    flowRunner("crearVegeta").run();
-    flowRunner("deleteWarriorFlow").run();
-    String payloadValue =  ((String) flowRunner("getGohan").run()
-            .getMessage()
-            .getPayload()
-            .getValue());
+    @Test
+    public void executeDeleteWarriorOperation() throws Exception {
+        flowRunner("crearZwarriorFlow").run();
+        flowRunner("crearZwarriorFlow2").run();
+        flowRunner("crearVegeta").run();
+        flowRunner("deleteWarriorFlow").run();
+        String payloadValue = ((String) flowRunner("getGohan").run()
+                .getMessage()
+                .getPayload()
+                .getValue());
 
-    assertThat(payloadValue, is("No se ha encontrado el warrior Gohan"));
-  }
+        assertThat(payloadValue, is("No se ha encontrado el warrior Gohan"));
+    }
 
-  @Test
-  public void executeRetrieveInfoOperation() throws Exception {
-    String payloadValue = ((String) flowRunner("retrieveInfoFlow")
-                                      .run()
-                                      .getMessage()
-                                      .getPayload()
-                                      .getValue());
-    assertThat(payloadValue, is("Using Configuration [configId] with Connection id [aValue:100]"));
-  }
+    @Test
+    public void executeRetrieveInfoOperation() throws Exception {
+        String payloadValue = ((String) flowRunner("retrieveInfoFlow")
+                .run()
+                .getMessage()
+                .getPayload()
+                .getValue());
+        assertThat(payloadValue, is("Using Configuration [configId] with Connection id [aValue:100]"));
+    }
 
 
 }
